0a276ad295af43ed80c4d18df32a4c8274719bed
BUVANESH R <94214803+buvaneshraina@users.noreply.github.com>
2022-08-11 19:56:25 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var x= Console.ReadLine();
+            Console.WriteLine("your value:" + x);
+        }
+    }
+}
